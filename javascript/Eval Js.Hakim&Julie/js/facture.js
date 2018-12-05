5f20f042f5eaf5e3af4c90fb23a9eec338cd6c63
JulieB <julie.buisson@lepoles.com>
2018-12-05 17:09:14 +0100

    C:/Program Files/Git/

--- javascript/Eval Js.Hakim&Julie/js/facture.js
+++ javascript/Eval Js.Hakim&Julie/js/facture.js
@@ -39,8 +39,7 @@ function Client(societe, nom, rue, code_postal_ville, numero) {
         this.code_postal_ville = code_postal_ville,
         this.numero = numero;
 }
-
-// Déclaration des nouveaux Clients
+// Déclaration des nouveaux clients
 
 var umbrella = ["Umbrella Corporation", "Umbrella", "7 rue du Hive", "66600 Racoon City", "01.34.56.28.51"];
 
@@ -52,7 +51,7 @@ var fujikawa = ["Fuji Corporation", "Fujikawa", "18 blv du pecheur", "18750 Mont
 
 var ubisoft = ["Ubisoft Paris Studio", "Ubisoft", "57 avenue du Boulgour", "28475 Funkytown", "01.37.78.71.95"];
 
-// Affichage des clients dans le 1er bloc (select)
+//Affichage des clients dans le 1er bloc (select)
 
 var select1 = document.getElementById('newClient');
 
@@ -62,7 +61,7 @@ select1.innerHTML += "<option>" + 'Umbrella' + "</option>";
 select1.innerHTML += "<option>" + 'Ubisoft' + "</option>";
 select1.innerHTML += "<option>" + 'Fujikawa' + "</option>";
 
-// Affichage des clients dans le 2eme bloc (select)
+//Affichage des clients dans le 2ème bloc (select)
 
 var select2 = document.getElementById('newExpediteur');
 
@@ -72,17 +71,17 @@ select2.innerHTML += "<option>" + 'Umbrella' + "</option>";
 select2.innerHTML += "<option>" + 'Blizzard' + "</option>";
 select2.innerHTML += "<option>" + 'Sony' + "</option>";
 
-// Var pour remplir les inputs du bas de la page
+//Var pour remplir les inputs du bas de la page
 
 var input = document.getElementsByTagName("input");
 var nomSociete = document.getElementById('redigerCheque');
 var tel = document.getElementById("telephone");
 var adresse = document.getElementById("adresse");
 
-// Remplissage du 1er select/option avec les valeurs de chaque clients
+//Remplisage du 1er select/option avec les valeurs de chaque clients
 
 select1.onclick = function () {
-    // Nouveau client "= false" pour pouvoir remplir les champs vides
+    //Nouveau client "=false" pour pouvoir remplir les champs vides
     if (select1.value == "Nouveau client") {
         input[5].value = "";
         input[5].disabled = false;
@@ -95,7 +94,7 @@ select1.onclick = function () {
         input[9].value = "";
         input[9].disabled = false;
     }
-    // Clients pré-enregistrés "= true" pour bloquer/empecher le changement de données
+    //Clients pré-enregistrés "=true" pour bloquer/empecher le changement de données
     else if (select1.value == "Ubisoft") {
         input[5].value = ubisoft[0];
         input[5].disabled = true;
@@ -107,7 +106,7 @@ select1.onclick = function () {
         input[8].disabled = true;
         input[9].value = ubisoft[4];
         input[9].disabled = true;
-        // If pour renvoyer les infos pré-enregistrées en bas de la page
+        //If pour renvoyer les infos pré-enregistrés en bas de la page
         if (select1.value == "Ubisoft") {
             nomSociete.value = ubisoft[0];
             tel.value = ubisoft[4];
@@ -189,7 +188,8 @@ select1.onclick = function () {
     }
 }
 
-// Remplissage du 2eme select/option avec les valeurs de chaque clients (pareil que le 1er bloc au dessus).
+
+//Remplissage du 2ème select/option avec les valeurs de chaque clients (pareil que le 1er bloc au dessus)
 
 select2.onclick = function () {
     if (select2.value == "Nouveau client") {
@@ -274,7 +274,7 @@ function Produits(code, libelle, prix) {
         this.prix = prix;
 }
 
-// Déclaration des 6 options (produits à vendre)
+//Déclaration des 6 options (produits à vendre)
 
 var option1 = ["#463652", "Pack PS4 Slim Black 4K 2To", 399.99];
 var option2 = ["#654251", "World of Warcraft Edition Collector", 369.99]
@@ -283,14 +283,14 @@ var option4 = ["#895463", "Assasin's Creed Odyssey", 69.99]
 var option5 = ["#254879", "Resident Evil VII BioHazard Collector", 89.99]
 var option6 = ["#325468", "Tekken 7 Edition Collector Deluxe Premium", 299.99]
 
-// Appel des différents selecteurs
+//Appel des différents selecteurs
 
 var sel = document.getElementById('produit');
 var sel2 = document.getElementById('produit2');
 var sel3 = document.getElementById('produit3');
 var sel4 = document.getElementById('produit4');
 
-// Affichage des options dans les selects
+//Affichage des options dans le select
 
 sel.innerHTML += "<option>" + "#463652" + "</option>";
 sel.innerHTML += "<option>" + "#654251" + "</option>";
@@ -320,8 +320,7 @@ sel4.innerHTML += "<option>" + "#895463" + "</option>";
 sel4.innerHTML += "<option>" + "#254879" + "</option>";
 sel4.innerHTML += "<option>" + "#325468" + "</option>";
 
-
-// Var pour 'appeler' chaque cases du tableau où vont s'afficher les informations relatives aux produits.
+//Var pour "appeler" chaques cases du tableau ou vont s'afficher les informations relatives aux produits
 
 var libe1 = document.getElementById('libe1');
 var prix1 = document.getElementById('prix1');
@@ -335,12 +334,12 @@ var prix3 = document.getElementById('prix3');
 var libe4 = document.getElementById('libe4');
 var prix4 = document.getElementById('prix4');
 
-// Fonction pour remplir le libellé et le prix des produits (select)
+//function pour remplir le libellé et le prix des produits (select)
 
 function Produits(sel, lib, select_valeur, prix, opt, i) {
     if (sel.value == select_valeur) {
         lib.innerHTML = opt[i];
-        i = i + 1;
+        i += 1;
         prix.innerHTML = opt[i];
     }
 };
@@ -381,7 +380,7 @@ sel4.onclick = function () {
     Produits(sel4, libe4, "#325468", prix4, option6, 1);
 };
 
-// Calcul du prix * la quantité pour retourner/remplir le montant
+// calcul du prix * la quantité pour retourner/remplir le montant
 
 var montant = document.getElementById('montant');
 var quantite = document.getElementById('nbr');
@@ -399,39 +398,83 @@ var montant4 = document.getElementById('montant4');
 var quantite4 = document.getElementById('nbr4');
 var prix4 = document.getElementById('prix4');
 
+
 function calcul(mont, quant, p) {
-   var x = Number(quant.value) * Number(p.innerHTML);
-   mont.innerHTML = x.toFixed(2);
-   sous()
+    var x = Number(quant.value) * Number(p.innerHTML);
+    mont.innerHTML = x.toFixed(2);
+    sous();
+    tva();
+    tt();
+    remise()
+}
+quantite.onclick = function () {
+    calcul(montant, quantite, prix);
+}
+quantite.onkeyup = function () {
+    calcul(montant, quantite, prix);
 }
 
-quantite.onclick = function(){
-   calcul(montant, quantite, prix);
+quantite2.onclick = function () {
+    calcul(montant2, quantite2, prix2);
+}
+quantite2.onkeyup = function () {
+    calcul(montant2, quantite2, prix2);
 }
 
-quantite2.onkeyup = function(){
-   calcul(montant2, quantite2, prix2);
+quantite3.onclick = function () {
+    calcul(montant3, quantite3, prix3);
+}
+quantite3.onkeyup = function () {
+    calcul(montant3, quantite3, prix3);
 }
 
-quantite3.onkeyup = function(){
-   calcul(montant3, quantite3, prix3);
+quantite4.onclick = function () {
+    calcul(montant4, quantite4, prix4);
+}
+quantite4.onkeyup = function () {
+    calcul(montant4, quantite4, prix4);
 }
 
-quantite4.onkeyup = function(){
-   calcul(montant4, quantite4, prix4);
-}
+// fin des calculs des produits
+
+// Partie en dessous du tableau : Sous-total, TVA, etc...
 
 var sousT = document.getElementById('ss');
 
-function sous(){
-   var x = Number(montant.innerHTML) + Number(montant2.innerHTML) + Number(montant3.innerHTML) +Number(montant4.innerHTML);
-   sousT.innerHTML = x.toFixed(2);
+function sous() {
+    var x = Number(montant.innerHTML) + Number(montant2.innerHTML) + Number(montant3.innerHTML) + Number(montant4.innerHTML);
+    sousT.innerHTML = x.toFixed(2);
 }
+
 var tv = document.getElementById('tva');
+var portM = document.getElementById('port');
 
-function tva(){
+function tva() {
+    var pht = Number(sousT.innerHTML);
+    var pm = Number(sousT.innerHTML);
+    tv.innerHTML = (pht * 20 / 100).toFixed(2);
+    portM.innerHTML = (pm * 10 / 100).toFixed(2);
+}
 
-};
+var ttc = document.getElementById('total');
+
+function tt() {
+    var totalT = Number(sousT.innerHTML) + Number(tv.innerHTML) + Number(portM.innerHTML);
+    ttc.innerHTML = totalT.toFixed(2);
+}
+
+var rem = document.getElementById('remise');
+var remise1 = document.getElementById('remise5');
+
+function remise() {
+    if (ttc.innerHTML >= 1000) {
+        rem.style.display = "block";
+        remise1.innerHTML = (ttc.innerHTML * 5 / 100).toFixed(2);
+    }
+    else if (h.innerHTML <= 1000) {
+        rem.style.display = "none";
+    }
+}
 
 
 //Boutton imprimer
